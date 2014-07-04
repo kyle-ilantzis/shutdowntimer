9a41dd4bd02ed93d427d44c41d234443edb920ad
kyle-ilantzis <ilantz.kyle@gmail.com>
2014-07-04 08:06:01 -0400

    fix build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-buildOptions = dict(packages = [], excludes = [], includes = ['tkwevents', 'ifcfg'])
+buildOptions = dict(packages = [], excludes = [], includes = [])
 
 import sys
 base = 'Win32GUI' if sys.platform=='win32' else None
